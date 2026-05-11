b68758b79ded566370ff502b51870e08e2d3ff7e
liem-iphone-2 <liem20k@gmail.com>
2026-05-11 13:19:52 +0000

    ip_2: cập nhật

--- ln.sh
+++ ln.sh
@@ -1,19 +1,2 @@
 #!/usr/bin/env bash
-set -euo pipefail
-
-BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# Load functions
-source "$BASE_DIR/ln_lang_nghiem.bash"
-
-# Nếu không truyền tham số thì vẫn chạy được
-ARGS=("$@")
-
-# Tên lệnh được gọi (ln hay lnk nếu tạo symlink)
-CMD="$(basename "$0")"
-
-if [[ "$CMD" == "lnk" ]]; then
-  lnk "${ARGS[@]}"
-else
-  ln "${ARGS[@]}"
-fi
+bash /root/ish_lang_nghiem/ln_lang_nghiem.bash "$@"
